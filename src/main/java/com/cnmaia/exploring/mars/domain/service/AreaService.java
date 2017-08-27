3b688c5b46f8cb1a9ea27a7631c551353cc95ba1
Caio Maia <caaiomaia@gmail.com>
2017-08-27 03:47:28 -0300

    rm HoverService, new methods in AreaService and added AreaService implementation class

--- src/main/java/com/cnmaia/exploring/mars/domain/service/AreaService.java
+++ src/main/java/com/cnmaia/exploring/mars/domain/service/AreaService.java
@@ -1,11 +1,15 @@
 package com.cnmaia.exploring.mars.domain.service;
 
 import com.cnmaia.exploring.mars.domain.model.Area;
-import com.cnmaia.exploring.mars.domain.model.Coordinate;
+import com.cnmaia.exploring.mars.domain.model.Hover;
+
+import java.util.Set;
 
 /**
  * Created by cmaia on 8/27/17.
  */
 public interface AreaService {
-    Area create(Coordinate upperRightCoordinate);
+    Area deployHover(Area area, Hover hover);
+    Area deployHovers(Area area, Set<Hover> hovers);
+    Area executeHoversInstructions(Area area);
 }
---
+++ src/main/java/com/cnmaia/exploring/mars/domain/service/impl/AreaServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cnmaia.exploring.mars.domain.service.impl;
+
+import com.cnmaia.exploring.mars.domain.model.Area;
+import com.cnmaia.exploring.mars.domain.model.Hover;
+import com.cnmaia.exploring.mars.domain.service.AreaService;
+
+import java.util.Set;
+
+/**
+ * Created by cmaia on 8/27/17
+ */
+public class AreaServiceImpl implements AreaService {
+
+    @Override
+    public Area deployHover(Area area, Hover hover) {
+        return null;
+    }
+
+    @Override
+    public Area deployHovers(Area area, Set<Hover> hovers) {
+        return null;
+    }
+
+    @Override
+    public Area executeHoversInstructions(Area area) {
+        return null;
+    }
+}
