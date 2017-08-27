c4d6b23d91d9de1536d60b1b483af1b2d8b3e8c3
Caio Maia <caaiomaia@gmail.com>
2017-08-27 02:55:47 -0300

    Removed move method from hover service

--- src/main/java/com/cnmaia/exploring/mars/domain/service/HoverService.java
+++ src/main/java/com/cnmaia/exploring/mars/domain/service/HoverService.java
@@ -3,12 +3,10 @@ package com.cnmaia.exploring.mars.domain.service;
 import com.cnmaia.exploring.mars.domain.model.Coordinate;
 import com.cnmaia.exploring.mars.domain.model.Direction;
 import com.cnmaia.exploring.mars.domain.model.Hover;
-import com.cnmaia.exploring.mars.domain.model.Instruction;
 
 /**
  * Created by cmaia on 8/27/17.
  */
 public interface HoverService {
     Hover deploy(Coordinate initialLocation, Direction initialFacingDirection);
-    void move(Hover hover, Instruction instruction);
 }
