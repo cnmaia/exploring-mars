e5ca2cb929740d967d3bbc8ef3a53c823cbbe3d1
Caio Maia <caaiomaia@gmail.com>
2017-09-01 03:47:12 -0300

    Added spring boot starter class and properties

--- src/main/java/com/cnmaia/exploring/mars/Application.java
+++ src/main/java/com/cnmaia/exploring/mars/Application.java
@@ -1,10 +1,14 @@
 package com.cnmaia.exploring.mars;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by cmaia on 8/26/17
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
